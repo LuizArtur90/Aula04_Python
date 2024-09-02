67e919254891aba3bf9709230b091efe230032b2
Luiz Artur <luiz.lamoglia@gmail.com>
2024-09-02 14:01:31 -0300

    exemplo dict

---
+++ dictionary.py
@@ -0,0 +1,30 @@
+produto1:dict = {
+    "nome":"Bola",
+    "peso":0.7,
+    "cor":"branca",
+    "tipo":"Futevolei",
+}
+
+produto2:dict = {
+    "nome":"Garrafa",
+    "peso":0.4,
+    "cor":"azul",
+    "tipo":"Futebol",
+}
+
+produto3:dict = {
+    "nome":"Rede",
+    "peso":2.9,
+    "cor":"Preta",
+    "tipo":"Futevolei",
+}
+
+carrinho:list = []
+carrinho.append(produto1)
+carrinho.append(produto2)
+carrinho.append(produto3)
+
+print(produto1)
+print(produto2)
+print(produto3)
+
