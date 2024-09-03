b244bf5a314602e459bb4fd318a7dc5d393a10ef
Luiz Artur <luiz.lamoglia@gmail.com>
2024-09-03 10:41:18 -0300

    add exerc

---
+++ exercicios.py
@@ -0,0 +1,20 @@
+#Dada uma lista de idades, filtrar apenas aquelas que são maiores ou iguais a 18.
+
+#idades = [22, 15, 30, 17, 18]
+#idades_validas = [idade for idade in idades if idade >= 18]
+
+#print(idades_validas)
+
+
+#Dada uma lista de dicionários representando pessoas, ordená-las pelo nome.
+#pessoas:list = [
+
+#    {"nome":"Manu", "idade":30, "cidade":"Arroio do Meio"},
+#    {"nome":"Luiz", "idade":34, "cidade":"Itajubá"},
+#    {"nome":"Paçoca", "idade":2, "cidade":"Biguaçu"},
+#]
+
+#pessoas.sort(key=lambda pessoas: pessoas["idade"])
+#print(pessoas)
+
+
